107fc2605dfa4abc3267b339b7518546a2acd3ce
rjimin <jimin.r@outlook.com>
2024-09-26 22:02:36 +0100

    Added CV

--- src/containers/greeting/Greeting.js
+++ src/containers/greeting/Greeting.js
@@ -41,8 +41,8 @@ export default function Greeting() {
                 <Button text="Contact me" href="#contact" />
                 {greeting.resumeLink && (
                   <a
-                    href={require("./resume.pdf")}
-                    download="Resume.pdf"
+                    href={require("./Jimin_Ryu_CV.pdf")}
+                    download="Jimin_Ryu_CV.pdf"
                     className="download-link-button"
                   >
                     <Button text="Download my resume" />
